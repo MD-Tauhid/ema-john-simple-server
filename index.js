75c834c8a4ebac4295c214cbab5b7a828d9362d8
Tauhid <tauhid.swe@gmail.com>
2023-03-15 12:33:46 +0600

    create an api for emaJhon products

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000 ;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+//mongoDb setup
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ybh5qdc.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        const productsCollection = client.db("emaJohn").collection('products');
+        
+        app.get('/products', async(req, res)=>{
+            const query = {};
+            const cursor = productsCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+    }
+    finally{
+
+    }
+}
+run().catch(err => console.error(err));
+
+app.get('/', (req, res)=>{
+    res.send('Ema-john server is running')
+});
+
+app.listen(port, () =>{
+    console.log(`ema john server is running on port:${port}`)
+})
