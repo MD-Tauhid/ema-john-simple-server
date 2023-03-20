598d8de82f8e66dbf5cac56dbe31146f946eeb5b
Tauhid <tauhid.swe@gmail.com>
2023-03-20 15:40:15 +0600

    made an post api for load data with body

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
-const port = process.env.PORT || 5000 ;
+const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
@@ -13,31 +13,40 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ybh5qdc.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
+async function run() {
+    try {
         const productsCollection = client.db("emaJohn").collection('products');
-        
-        app.get('/products', async(req, res)=>{
+
+        app.get('/products', async (req, res) => {
             const page = parseInt(req.query.page);
             const size = parseInt(req.query.size);
             const query = {};
             const cursor = productsCollection.find(query);
-            const products = await cursor.skip(page*size).limit(size).toArray();
+            const products = await cursor.skip(page * size).limit(size).toArray();
             const count = await productsCollection.estimatedDocumentCount();
-            res.send({count, products});
+            res.send({ count, products });
         });
 
+        app.post('/productsByIds', async (req, res) => {
+            const ids = req.body;
+            const objectIds = ids.map(id => new ObjectId(id));
+            const query = { _id: { $in: objectIds } }
+            const cursor = productsCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+
     }
-    finally{
+    finally {
 
     }
 }
 run().catch(err => console.error(err));
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('Ema-john server is running')
 });
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log(`ema john server is running on port:${port}`)
 })
